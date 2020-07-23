8140b4834406b884a40694340ac450635364f5a6
Annabelle Thomas Taylor <ataylor@mapc.org>
2020-07-23 15:53:52 -0400

    Convert from create-react-app to GatsbyJS

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Sidebar from '../components/Sidebar';
+import Map from '../components/Map';
+
+export default function Home() {
+  return (
+    <>
+      <Sidebar />
+      <Map />
+    </>
+  )
+}
