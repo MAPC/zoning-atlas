6ea33ce2fb066ed0b6f99e5bcd9120a50dfaecf6
Annabelle Thomas Taylor <ataylor@mapc.org>
2020-09-09 10:15:12 -0400

    Refactor multiselect to use filterValue directly

--- src/components/MultiSelectColumnFilter.jsx
+++ src/components/MultiSelectColumnFilter.jsx
@@ -1,26 +1,8 @@
-import React, { useState, useEffect } from "react"
-
-function toggleMunicipality(e, selectedMunis, setSelectedMunis) {
-  e.currentTarget.querySelector('.filters__button').classList.toggle('filters__button--active');
-  e.currentTarget.classList.toggle('filters__option--active');
-  e.currentTarget.querySelector('.filters__value').classList.toggle('filters__option--active');
-  const muni = e.currentTarget.querySelector('.filters__value').innerText;
-  if (selectedMunis === undefined) {
-    setSelectedMunis([muni])
-  } else if (!selectedMunis.includes(muni)) {
-    setSelectedMunis(selectedMunis.concat([muni]))
-  } else if (selectedMunis.includes(muni) && selectedMunis.length > 1) {
-    const selectedIndex = selectedMunis.indexOf(muni)
-    setSelectedMunis(selectedMunis.slice(0, selectedIndex).concat(selectedMunis.slice(selectedIndex+1)))
-  } else {
-    setSelectedMunis(undefined)
-  }
-}
+import React from "react"
 
 function MultiSelectColumnFilter({
-  column: { setFilter, preFilteredRows, id },
+  column: { filterValue, setFilter, preFilteredRows, id },
 }) {
-  const [selectedMunis, setSelectedMunis] = useState()
   const options = React.useMemo(() => {
     const options = new Set()
     preFilteredRows.forEach(row => {
@@ -29,16 +11,28 @@ function MultiSelectColumnFilter({
     return [...options.values()]
   }, [id, preFilteredRows])
 
-  useEffect(() => {
-    setFilter(selectedMunis)
-  }, [selectedMunis])
   return (
     <ul className="filters__list">
       {options.map((option, i) => (
         <li
           className="filters__option"
           key={`option-${i}`}
-          onClick={(e) => toggleMunicipality(e, selectedMunis, setSelectedMunis)}>
+          onClick={(e) => {
+            e.currentTarget.querySelector('.filters__button').classList.toggle('filters__button--active');
+            e.currentTarget.classList.toggle('filters__option--active');
+            e.currentTarget.querySelector('.filters__value').classList.toggle('filters__option--active');
+            const muni = e.currentTarget.querySelector('.filters__value').innerText;
+            if (filterValue === undefined) {
+              setFilter([muni])
+            } else if (!filterValue.includes(muni)) {
+              setFilter(filterValue.concat([muni]))
+            } else if (filterValue.includes(muni) && filterValue.length > 1) {
+              const selectedIndex = filterValue.indexOf(muni)
+              setFilter(filterValue.slice(0, selectedIndex).concat(filterValue.slice(selectedIndex+1)))
+            } else {
+              setFilter(undefined)
+            }
+          }}>
           <span className="filters__value">{option}</span>
           <button className="filters__button">+</button>
         </li>
