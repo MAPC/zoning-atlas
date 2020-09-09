3a242f9e7db30ee4bcbb968c262fe433c9162f49
Annabelle Thomas Taylor <ataylor@mapc.org>
2020-09-09 14:14:25 -0400

    Style filter categories

--- src/components/TableFilters.jsx
+++ src/components/TableFilters.jsx
@@ -1,15 +1,23 @@
 import React, { useState } from "react"
 
+function toggleCategory(category, visibleFilter, setVisibleFilter) {
+  console.log("Old category: " + visibleFilter)
+  console.log("New Category: " + category)
+  document.getElementById(category).classList.add('filters-panel__category--active')
+  document.getElementById(visibleFilter).classList.remove('filters-panel__category--active')
+  setVisibleFilter(category)
+}
+
 const TableFilters = ({headerGroups}) => {
   const [visibleFilter, setVisibleFilter] = useState('muni')
 
   return (
-    <aside className='filters'>
-      <h2>Filters</h2>
-      <div className="filters__wrapper filters__wrapper--tabular">
-        <ul className="list__filters">
-          <li id="muni" onClick={() => {setVisibleFilter('muni')}}>Town/City</li>
-          <li id="zoName" onClick={() => {setVisibleFilter('zoName')}}>Zoning Name</li>
+    <aside className='filters-panel'>
+      <h2 className="filters-panel__header">Filters</h2>
+      <div className="filters-panel__body">
+        <ul className="filters-panel__categories">
+          <li id="muni" className="filters-panel__category filters-panel__category--active" onClick={() => toggleCategory('muni', visibleFilter, setVisibleFilter)}>Town/City</li>
+          <li id="zoName" className="filters-panel__category" onClick={() => toggleCategory('zoName', visibleFilter, setVisibleFilter)}>Zoning Name</li>
         </ul>
         <div className="filters__panel">
           { headerGroups[0].headers.find(header => header.id === visibleFilter).render('Filter') }
