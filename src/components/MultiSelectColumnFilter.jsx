e4aafc3791ed8fb48f2e314d84134e1d233ed02e
Annabelle Thomas Taylor <ataylor@mapc.org>
2020-09-09 10:35:29 -0400

    Add search for municipality
    * Styling for selected elements affected; if the first value is selected (say, Acton) and then a substring is searched, the first value of the results will display “selected” styles

--- src/components/MultiSelectColumnFilter.jsx
+++ src/components/MultiSelectColumnFilter.jsx
@@ -1,8 +1,9 @@
-import React from "react"
+import React, { useState } from "react"
 
 function MultiSelectColumnFilter({
   column: { filterValue, setFilter, preFilteredRows, id },
 }) {
+  const [searchString, setSearch] = useState('');
   const options = React.useMemo(() => {
     const options = new Set()
     preFilteredRows.forEach(row => {
@@ -10,34 +11,36 @@ function MultiSelectColumnFilter({
     })
     return [...options.values()]
   }, [id, preFilteredRows])
-
   return (
-    <ul className="filters__list">
-      {options.map((option, i) => (
-        <li
-          className="filters__option"
-          key={`option-${i}`}
-          onClick={(e) => {
-            e.currentTarget.querySelector('.filters__button').classList.toggle('filters__button--active');
-            e.currentTarget.classList.toggle('filters__option--active');
-            e.currentTarget.querySelector('.filters__value').classList.toggle('filters__option--active');
-            const muni = e.currentTarget.querySelector('.filters__value').innerText;
-            if (filterValue === undefined) {
-              setFilter([muni])
-            } else if (!filterValue.includes(muni)) {
-              setFilter(filterValue.concat([muni]))
-            } else if (filterValue.includes(muni) && filterValue.length > 1) {
-              const selectedIndex = filterValue.indexOf(muni)
-              setFilter(filterValue.slice(0, selectedIndex).concat(filterValue.slice(selectedIndex+1)))
-            } else {
-              setFilter(undefined)
-            }
-          }}>
-          <span className="filters__value">{option}</span>
-          <button className="filters__button">+</button>
-        </li>
-      ))}
-    </ul>
+    <>
+      <input className="filters__search" type="text" placeholder="Type here to search filters by Town/City" onChange={(e) => setSearch(e.target.value.toLowerCase())} />
+      <ul className="filters__list">
+        {options.filter((option) => option.toLowerCase().includes(searchString)).map((option, i) => (
+          <li
+            className="filters__option"
+            key={`option-${i}`}
+            onClick={(e) => {
+              e.currentTarget.querySelector('.filters__button').classList.toggle('filters__button--active');
+              e.currentTarget.classList.toggle('filters__option--active');
+              e.currentTarget.querySelector('.filters__value').classList.toggle('filters__option--active');
+              const muni = e.currentTarget.querySelector('.filters__value').innerText;
+              if (filterValue === undefined) {
+                setFilter([muni])
+              } else if (!filterValue.includes(muni)) {
+                setFilter(filterValue.concat([muni]))
+              } else if (filterValue.includes(muni) && filterValue.length > 1) {
+                const selectedIndex = filterValue.indexOf(muni)
+                setFilter(filterValue.slice(0, selectedIndex).concat(filterValue.slice(selectedIndex+1)))
+              } else {
+                setFilter(undefined)
+              }
+            }}>
+            <span className="filters__value">{option}</span>
+            <button className="filters__button">+</button>
+          </li>
+        ))}
+      </ul>
+    </>
   )
 }
 
