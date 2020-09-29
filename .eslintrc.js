ab3c8e908d94dd70bdaa4af7c37dfdb81c18f6d8
Annabelle Thomas Taylor <ataylor@mapc.org>
2020-09-29 11:08:44 -0400

    Add default MAPC eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  globals: {
+    __PATH_PREFIX__: true,
+  },
+  "extends": "airbnb",
+    "plugins": [
+        "react",
+        "jsx-a11y",
+        "import"
+    ],
+    "env": {
+      "browser": true
+    }
+}
