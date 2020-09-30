d379cee884ec695198098e1c768d52bc34edcf9a
Annabelle Thomas Taylor <ataylor@mapc.org>
2020-09-30 15:22:51 -0400

    Use React testing-library for component testing

--- src/components/filters/FilterOptionRow.jsx
+++ src/components/filters/FilterOptionRow.jsx
@@ -1,19 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 const FilterOptionRow = ({
   filterValue, index, option, setFilter,
 }) => {
+  const [isActive, setActiveRow] = useState(false);
   const handleSelection = () => {
     if (filterValue === undefined) {
       setFilter([option]);
+      setActiveRow(true);
     } else if (!filterValue.includes(option)) {
       setFilter(filterValue.concat([option]));
+      setActiveRow(true);
     } else if (filterValue.includes(option) && filterValue.length > 1) {
       const selectedIndex = filterValue.indexOf(option);
       setFilter(filterValue.slice(0, selectedIndex).concat(filterValue.slice(selectedIndex + 1)));
+      setActiveRow(false);
     } else {
       setFilter(undefined);
+      setActiveRow(false);
     }
   };
 
@@ -22,7 +27,7 @@ const FilterOptionRow = ({
       <div
         tabIndex="0"
         data-value={option}
-        className={filterValue && filterValue.includes(option) ? 'filters__option filters__option--active' : 'filters__option'}
+        className={isActive ? 'filters__option filters__option--active' : 'filters__option'}
         key={`option-${index}`}
         role="button"
         onKeyDown={(e) => {
@@ -32,8 +37,8 @@ const FilterOptionRow = ({
         }}
         onClick={() => handleSelection()}
       >
-        <span className={filterValue && filterValue.includes(option) ? 'filters__value filters__value--active' : 'filters__value'}>{option}</span>
-        <span className={filterValue && filterValue.includes(option) ? 'filters__button filters__button--active' : 'filters__button'}>+</span>
+        <span className={isActive ? 'filters__value filters__value--active' : 'filters__value'}>{option}</span>
+        <span className={isActive ? 'filters__button filters__button--active' : 'filters__button'}>+</span>
       </div>
     </li>
   );
--- src/components/filters/__tests__/FilterOptionRow.js
+++ src/components/filters/__tests__/FilterOptionRow.js
@@ -1,14 +1,28 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { describe, it, expect } from '@jest/globals';
 
 import FilterOptionRow from '../FilterOptionRow';
 
 describe('Filter option (row)', () => {
-  it('renders correctly', () => {
-    const tree = renderer
-      .create(<FilterOptionRow index={0} option="Acton" setFilter={() => null}/>)
-      .toJSON();
-    expect(tree).toMatchSnapshot();
+  it('renders', () => {
+    render(<FilterOptionRow index={0} option="Acton" setFilter={() => null} />);
+  });
+
+  it('displays the correct value', () => {
+    render(<FilterOptionRow index={0} option="Acton" setFilter={() => null} />);
+    expect(screen.getByText('Acton')).toBeInTheDocument();
+  });
+
+  it('becomes active on option click', () => {
+    render(<FilterOptionRow index={0} option="Acton" setFilter={() => null} />);
+    fireEvent.click(screen.getByText('Acton'));
+    expect(screen.getByText('Acton')).toHaveClass('filters__value--active');
+  });
+
+  it('turns inactive on double option click', () => {
+    render(<FilterOptionRow index={0} option="Acton" setFilter={() => null} />);
+    fireEvent.dblClick(screen.getByText('Acton'));
+    expect(screen.getByText('Acton')).not.toHaveClass('filters__value--active');
   });
 });
