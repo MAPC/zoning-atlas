29c3cf7affdbaedac2e96d080e64072ed4da791c
Annabelle Thomas Taylor <ataylor@mapc.org>
2020-10-29 11:18:18 -0400

    Reset homepage to data view

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,37 +1,105 @@
-import React from "react"
-import Storymap from '../components/map/Storymap';
-import MapSliderContainer from '../components/map/MapSliderContainer';
-import { Helmet } from "react-helmet"
-import '../styles/app.scss'
+import React, { useMemo, useState } from 'react';
+import { Helmet } from 'react-helmet';
+import {
+  useTable, usePagination, useSortBy, useFilters,
+} from 'react-table';
+import { graphql } from 'gatsby';
+import setZoneUse from '../utils/setZoneUse';
+import setMultifamily from '../utils/setMultiFamily';
+import { multiple, inclusiveOr, numericRange } from '../utils/setFilterTypes';
+import setColumns from '../utils/setColumns';
+import TableFilters from '../components/filters/TableFilters';
+import TableWrapper from '../components/table/TableWrapper';
+import LeafletMap from '../components/map/LeafletMap';
+import '../styles/app.scss';
 
-export default function Home() {
+export default function Tabular({ data }) {
+  const filterTypes = React.useMemo(() => ({
+    multiple,
+    inclusiveOr,
+    numericRange,
+  }), []);
+
+  const defaultColumn = React.useMemo(() => ({
+    Filter: () => <div />,
+  }), []);
+
+  data = useMemo(() => data.postgres.allDraftDatabasesList.map((row) => ({
+    muni: row.muni,
+    zoName: row.zoName ? row.zoName.split(',') : [''],
+    zoUsety: setZoneUse(row.zoUsety),
+    multifam: setMultifamily(row.multifam),
+    plcEff: row.plcEff * 100,
+    minlotsize: row.minlotsize,
+    pctlotcov: row.pctlotcov * 100,
+    lApDu: row.lApDu,
+    dUpAc: row.dUpAc,
+    far: row.far,
+    mxhtEff: row.mxhtEff,
+    mxflEff: row.mxflEff,
+  })), [data.postgres.allDraftDatabasesList]);
+
+  const columns = useMemo(() => setColumns, []);
+  const reactTable = useTable({
+    columns, data, initialState: { pageSize: 10 }, defaultColumn, filterTypes, defaultCanFilter: false,
+  }, useFilters, useSortBy, usePagination);
+
+  const [view, setView] = useState('spatial');
+
   return (
     <>
       <Helmet
-        title={"Zoning Atlas"}
+        title="Zoning Atlas - Tabular Data"
       >
-        <link href="https://api.mapbox.com/mapbox-gl-js/v1.11.1/mapbox-gl.css" rel="stylesheet" />
-        <link
-          rel="stylesheet"
-          href="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-compare/v0.4.0/mapbox-gl-compare.css"
-          type="text/css"
-          />
+        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+        integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
+        crossorigin=""/>
+        <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
+        integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
+        crossorigin=""></script>
       </Helmet>
       <h1>Zoning Atlas</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras at felis vitae arcu aliquam laoreet ut vitae turpis. Cras elementum diam sit amet ex fermentum congue. Cras mattis tristique dolor at vulputate. Phasellus aliquam enim iaculis cursus aliquet. Interdum et malesuada fames ac ante ipsum primis in faucibus. Etiam sagittis, eros efficitur vulputate ultrices, turpis libero vulputate nunc, a ultricies ipsum mauris a mi. Maecenas pretium tincidunt faucibus. Pellentesque vel metus lectus. Curabitur tortor eros, blandit eget neque in, elementum sodales leo.
-      </p>
-      <Storymap />
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras at felis vitae arcu aliquam laoreet ut vitae turpis. Cras elementum diam sit amet ex fermentum congue. Cras mattis tristique dolor at vulputate. Phasellus aliquam enim iaculis cursus aliquet. Interdum et malesuada fames ac ante ipsum primis in faucibus. Etiam sagittis, eros efficitur vulputate ultrices, turpis libero vulputate nunc, a ultricies ipsum mauris a mi. Maecenas pretium tincidunt faucibus. Pellentesque vel metus lectus. Curabitur tortor eros, blandit eget neque in, elementum sodales leo.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras at felis vitae arcu aliquam laoreet ut vitae turpis. Cras elementum diam sit amet ex fermentum congue. Cras mattis tristique dolor at vulputate. Phasellus aliquam enim iaculis cursus aliquet. Interdum et malesuada fames ac ante ipsum primis in faucibus. Etiam sagittis, eros efficitur vulputate ultrices, turpis libero vulputate nunc, a ultricies ipsum mauris a mi. Maecenas pretium tincidunt faucibus. Pellentesque vel metus lectus. Curabitur tortor eros, blandit eget neque in, elementum sodales leo.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras at felis vitae arcu aliquam laoreet ut vitae turpis. Cras elementum diam sit amet ex fermentum congue. Cras mattis tristique dolor at vulputate. Phasellus aliquam enim iaculis cursus aliquet. Interdum et malesuada fames ac ante ipsum primis in faucibus. Etiam sagittis, eros efficitur vulputate ultrices, turpis libero vulputate nunc, a ultricies ipsum mauris a mi. Maecenas pretium tincidunt faucibus. Pellentesque vel metus lectus. Curabitur tortor eros, blandit eget neque in, elementum sodales leo.
-      </p>
-      <MapSliderContainer />
+      <button
+        type="button"
+        onClick={() => setView('tabular')}
+      >
+        Tabular
+      </button>
+      <button
+        type="button"
+        onClick={() => setView('spatial')}
+      >
+        Spatial
+      </button>
+      <div className="tabular-data">
+        <TableFilters
+          headerGroups={reactTable.headerGroups}
+        />
+        {view === 'tabular'
+          ? <TableWrapper reactTable={reactTable} />
+          : <LeafletMap reactTable={reactTable} />}
+      </div>
     </>
-  )
+  );
 }
+
+export const data = graphql`
+  query {
+    postgres {
+      allDraftDatabasesList {
+        muni
+        zoName
+        zoUsety
+        multifam
+        plcEff
+        minlotsize
+        pctlotcov
+        lApDu
+        dUpAc
+        far
+        mxhtEff
+        mxflEff
+      }
+    }
+  }
+`;
