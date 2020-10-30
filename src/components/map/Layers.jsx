5aa5a8c86b45b4b5fad79db486ea5d6f4f410c32
Annabelle Thomas Taylor <ataylor@mapc.org>
2020-10-30 14:27:52 -0400

    Change opacity on zoom level

--- src/components/map/Layers.jsx
+++ src/components/map/Layers.jsx
@@ -1,38 +1,46 @@
-/* eslint-disable arrow-body-style */
-import React from 'react';
-import { FeatureLayer } from 'react-esri-leaflet/v2';
+import React, { useState } from 'react';
+import { FeatureLayer } from 'react-esri-leaflet';
+import { useMap, useMapEvent } from 'react-leaflet';
 
-const Layers = ({ mapZoom }) => {
-  if (typeof window !== 'undefined') {
-    return (
-      <>
-        <FeatureLayer
-          url="https://geo.mapc.org/server/rest/services/gisdata/ZoningKitchenSinkTest_v03/MapServer/0"
-          simplifyFactor={0.9}
-          style={{
-            weight: 0.5,
-            opacity: 1,
-          }}
-        />
-        <FeatureLayer
-          url="https://geo.mapc.org/server/rest/services/transportation/MBTA/MapServer/2"
-          style={(e) => {
-            if (mapZoom > 9) {
-              console.log('Zoomed in');
-              return {
-                color: 'green',
-              };
-            }
-            console.log('Default zoom');
+const Layers = () => {
+  const mapRef = useMap();
+  const [zoom, setZoom] = useState(mapRef.getZoom());
+
+  useMapEvent('zoomend', () => {
+    setZoom(mapRef.getZoom());
+  });
+
+  return (
+    <>
+      <FeatureLayer
+        url="https://geo.mapc.org/server/rest/services/gisdata/ZoningKitchenSinkTest_v03/MapServer/0"
+        simplifyFactor={0.9}
+        style={() => {
+          if (zoom > 9) {
             return {
-              color: 'red',
+              opacity: 0,
             };
-          }}
-        />
-      </>
-    );
-  }
-  return null;
+          }
+          return {
+            color: 1,
+          };
+        }}
+      />
+      <FeatureLayer
+        url="https://geo.mapc.org/server/rest/services/transportation/MBTA/MapServer/2"
+        style={() => {
+          if (zoom > 9) {
+            return {
+              opacity: 1,
+            };
+          }
+          return {
+            opacity: 0,
+          };
+        }}
+      />
+    </>
+  );
 };
 
 export default Layers;
--- src/components/map/LeafletMap.jsx
+++ src/components/map/LeafletMap.jsx
@@ -1,30 +1,21 @@
 /* eslint-disable arrow-body-style */
 import React, { useState } from 'react';
-import { Map, TileLayer } from 'react-leaflet';
+import { MapContainer, TileLayer } from 'react-leaflet';
 import Layers from './Layers';
 
-const LeafletMap = () => {
-  const [zoom, setZoom] = useState(9);
-  if (typeof window !== 'undefined') {
-    return (
-      <Map
-        center={[42.3317, -71.0408]}
-        zoom={zoom}
-        maxZoom={14}
-        minZoom={9}
-        onViewportChanged={(map) => setZoom(map.zoom)}
-      >
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-        />
-        <Layers
-          mapZoom={zoom}
-        />
-      </Map>
-    );
-  }
-  return null;
-};
+const LeafletMap = () => (
+  <MapContainer
+    center={[42.3317, -71.0408]}
+    zoom={9}
+    maxZoom={14}
+    minZoom={9}
+  >
+    <TileLayer
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+    />
+    <Layers />
+  </MapContainer>
+);
 
 export default LeafletMap;
