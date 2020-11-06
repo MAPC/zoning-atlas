f1112dfeb6960ab9284e582835a602a7bcdc5759
Annabelle Thomas Taylor <ataylor@mapc.org>
2020-11-06 15:05:05 -0500

    Display hover effect on header nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,38 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'gatsby';
+import logo from '../images/logo.svg';
 
-function setLinkClasses(currentPage, argPage) {
-  if (currentPage === argPage) {
+function setLinkClasses(urlPath, pageName, hoveredPage = false) {
+  if (hoveredPage) {
+    console.log(hoveredPage)
+    return 'navigation__link navigation__link--active';
+  }
+  if (urlPath === pageName) {
     return 'navigation__link navigation__link--active';
   }
   return 'navigation__link';
 }
 
 const Header = () => {
-  const page = typeof window !== 'undefined' ? window.location.pathname : '';
+  const path = typeof window !== 'undefined' ? window.location.pathname : '';
+  const [activePage, setActivePage] = useState(path);
   return (
-  <header className="header">
-    <h1 className="header__title">Zoning Atlas</h1>
-    <nav className="header__navigation">
-      <ul className="navigation__list">
-        <li className="navigation__item">
-          <Link to="/" className={setLinkClasses(page, '/')}>
-            Data
-          </Link>
-        </li>
-        <li className="navigation__item">
-          <Link to="/reports" className={setLinkClasses(page, '/reports')}>
-            Reports
-          </Link>
-        </li>
-        <li className="navigation__item">
-          <Link to="/about" className={setLinkClasses(page, '/about')}>
-            About
-          </Link>
-        </li>
-      </ul>
-    </nav>
-  </header>
-);
-  }
+    <header className="header">
+      <div className="header__logo-wrapper">
+        <img src={logo} className="header__logo" />
+        <h1 className="header__title">Zoning Atlas</h1>
+      </div>
+      <nav className="header__navigation">
+        <ul className="navigation__list">
+          <li className="navigation__item">
+            <Link
+              to="/"
+              className={activePage === '/' ? 'navigation__link navigation__link--active' : 'navigation__link'}
+              onMouseEnter={() => setActivePage('/')}
+              onMouseLeave={() => setActivePage(path)}
+            >
+              Data
+            </Link>
+          </li>
+          <li className="navigation__item">
+            <Link
+              to="/reports"
+              className={activePage === '/reports' ? 'navigation__link navigation__link--active' : 'navigation__link'}
+              onMouseEnter={() => setActivePage('/reports')}
+              onMouseLeave={() => setActivePage(path)}
+            >
+              Reports
+            </Link>
+          </li>
+          <li className="navigation__item">
+            <Link
+              to="/about"
+              className={activePage === '/about' ? 'navigation__link navigation__link--active' : 'navigation__link'}
+              onMouseEnter={() => setActivePage('/about')}
+              onMouseLeave={() => setActivePage(path)}
+            >
+              About
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 export default Header;
