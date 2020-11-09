856b56dbde298b4196484b9f19bb3342ac04e34f
Annabelle Thomas Taylor <ataylor@mapc.org>
2020-11-09 13:45:51 -0500

    Add phosphor icons for Export icon

--- src/components/data/ExportMenu.jsx
+++ src/components/data/ExportMenu.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import { Link } from 'phosphor-react';
 
 const ExportMenu = ({ view, setView }) => (
   <aside className="export-menu__wrapper">
     <span className="data-options__title export-menu__title">Export</span>
-    <div className="exports-menu__options-wrapper">
+    <div className="export-menu__options-wrapper">
       <button className="button" type="button">.shp</button>
       <button className="button" type="button">.csv</button>
-      <button className="button" type="button">link</button>
+      <button className="button button--icon" type="button">
+        <Link
+          size="1rem"
+          weight="bold"
+        />
+      </button>
     </div>
   </aside>
 );
