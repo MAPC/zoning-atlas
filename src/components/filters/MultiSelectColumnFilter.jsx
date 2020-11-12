2e2556b985cb5f580e0598707c817735d77e70dc
Annabelle Thomas Taylor <ataylor@mapc.org>
2020-11-12 11:57:38 -0500

    Adjust border-bottom for selected subview

--- src/components/filters/MultiSelectColumnFilter.jsx
+++ src/components/filters/MultiSelectColumnFilter.jsx
@@ -3,16 +3,23 @@ import { MagnifyingGlass, CheckSquareOffset } from 'phosphor-react';
 import SearchView from './SearchView';
 import ViewCurrentFilters from './ViewCurrentFilters';
 
+function setClasses(subview, button) {
+  if (subview === button) {
+    return 'filters__view filters__view--active';
+  }
+  return 'filters__view';
+}
+
 function MultiSelectColumnFilter({ column }) {
   const [subview, setSubview] = useState('search');
 
   return (
     <>
       <div className="filters__views">
-        <button className="filters__view" onClick={() => setSubview('search')} type="button">
+        <button className={setClasses(subview, 'search')} onClick={() => setSubview('search')} type="button">
           <MagnifyingGlass size={'1.5rem'} weight="bold" />
         </button>
-        <button className="filters__view" onClick={() => setSubview('selected')} type="button">
+        <button className={setClasses(subview, 'selected')} onClick={() => setSubview('selected')} type="button">
           <CheckSquareOffset size={'1.5rem'} weight="bold" />
           {' '}
           |
