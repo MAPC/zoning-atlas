ce31529465ca68416a06cb41edf9b29c864c2e73
Annabelle Thomas Taylor <ataylor@mapc.org>
2020-11-12 09:58:36 -0500

    Toggle between basemaps and add attribution

--- src/components/map/LeafletMap.jsx
+++ src/components/map/LeafletMap.jsx
@@ -1,26 +1,44 @@
 /* eslint-disable arrow-body-style */
 import React from 'react';
-import { MapContainer, TileLayer, ZoomControl } from 'react-leaflet';
-import EsriLeafletGeoSearch from "react-esri-leaflet/plugins/EsriLeafletGeoSearch";
+import {
+  MapContainer, TileLayer, ZoomControl, LayersControl,
+} from 'react-leaflet';
+import EsriLeafletGeoSearch from 'react-esri-leaflet/plugins/EsriLeafletGeoSearch';
 import Layers from './Layers';
 
 const LeafletMap = ({ reactTable }) => (
-  <MapContainer
-    center={[42.3317, -71.0408]}
-    zoom={9}
-    maxZoom={14}
-    minZoom={9}
-    zoomControl={false}
-  >
-    <TileLayer
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-    />
-    <Layers reactTable={reactTable} />
-    <ZoomControl position="bottomright" />
-    <EsriLeafletGeoSearch useMapBounds={false} position="topleft" />
-    <div>Save space for toggle</div>
-  </MapContainer>
+  <div className="map__wrapper">
+    <MapContainer
+      center={[42.3317, -71.0408]}
+      zoom={9}
+      maxZoom={14}
+      minZoom={9}
+      zoomControl={false}
+    >
+      <EsriLeafletGeoSearch useMapBounds={false} position="topleft" />
+      <LayersControl position="topleft" collapsed={false}>
+        <LayersControl.BaseLayer name="Streets" checked>
+          <TileLayer
+            url="https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaWhpbGwiLCJhIjoiY2plZzUwMTRzMW45NjJxb2R2Z2thOWF1YiJ9.szIAeMS4c9YTgNsJeG36gg"
+            attribution='© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>'
+            tileSize={512}
+            zoomOffset={-1}
+          />
+        </LayersControl.BaseLayer>
+        <LayersControl.BaseLayer name="Satellite">
+          <TileLayer
+            url="https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaWhpbGwiLCJhIjoiY2plZzUwMTRzMW45NjJxb2R2Z2thOWF1YiJ9.szIAeMS4c9YTgNsJeG36gg"
+            attribution='© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>'
+            tileSize={512}
+            zoomOffset={-1}
+          />
+        </LayersControl.BaseLayer>
+      </LayersControl>
+      {/* <Layers reactTable={reactTable} /> */}
+      <ZoomControl position="bottomright" />
+    </MapContainer>
+    <a href="http://mapbox.com/about/maps" className="map__watermark" target="_blank">Mapbox</a>
+  </div>
 );
 
 export default LeafletMap;
