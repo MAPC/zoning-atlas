89b5b403acdb147be037ec3591f1508bb013b2f5
Annabelle Thomas Taylor <ataylor@mapc.org>
2020-11-27 14:17:41 -0500

    Style tooltip list

--- src/components/map/LeafletMap.jsx
+++ src/components/map/LeafletMap.jsx
@@ -5,48 +5,49 @@ import {
 } from 'react-leaflet';
 import EsriLeafletGeoSearch from 'react-esri-leaflet/plugins/EsriLeafletGeoSearch';
 import Layers from './Layers';
-import Popup from './Popup';
+import ZoningPopup from './Popup';
 
 const LeafletMap = ({ reactTable }) => {
   const [selectedAttr, setSelected] = useState();
   const [popupLatLng, setLatLng] = useState();
   return (
-  <div className="map__wrapper">
-    <MapContainer
-      center={[42.3317, -71.0408]}
-      zoom={9}
-      maxZoom={14}
-      minZoom={9}
-      zoomControl={false}
-    >
-      <LayersControl position="topleft">
-        <LayersControl.BaseLayer name="Streets" checked>
-          <TileLayer
-            url="https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaWhpbGwiLCJhIjoiY2plZzUwMTRzMW45NjJxb2R2Z2thOWF1YiJ9.szIAeMS4c9YTgNsJeG36gg"
-            attribution='© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>'
-            tileSize={512}
-            zoomOffset={-1}
-          />
-        </LayersControl.BaseLayer>
-        <LayersControl.BaseLayer name="Satellite">
-          <TileLayer
-            url="https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaWhpbGwiLCJhIjoiY2plZzUwMTRzMW45NjJxb2R2Z2thOWF1YiJ9.szIAeMS4c9YTgNsJeG36gg"
-            attribution='© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>'
-            tileSize={512}
-            zoomOffset={-1}
-          />
-        </LayersControl.BaseLayer>
-      </LayersControl>
-      <EsriLeafletGeoSearch useMapBounds={false} position="topleft" />
-      <Layers reactTable={reactTable} setSelected={setSelected} setLatLng={setLatLng} />
-      {selectedAttr ? <Popup selectedAttr={selectedAttr} latLng={popupLatLng} /> : ''}
-      <ZoomControl position="bottomright" />
-    </MapContainer>
-    <a href="http://mapbox.com/about/maps" className="map__watermark" target="_blank">Mapbox</a>
-    <div className="map__legend">
-      Legend
+    <div className="map__wrapper">
+      <MapContainer
+        center={[42.3317, -71.0408]}
+        zoom={9}
+        maxZoom={14}
+        minZoom={9}
+        zoomControl={false}
+      >
+        <LayersControl position="topleft">
+          <LayersControl.BaseLayer name="Streets" checked>
+            <TileLayer
+              url="https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaWhpbGwiLCJhIjoiY2plZzUwMTRzMW45NjJxb2R2Z2thOWF1YiJ9.szIAeMS4c9YTgNsJeG36gg"
+              attribution='© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>'
+              tileSize={512}
+              zoomOffset={-1}
+            />
+          </LayersControl.BaseLayer>
+          <LayersControl.BaseLayer name="Satellite">
+            <TileLayer
+              url="https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaWhpbGwiLCJhIjoiY2plZzUwMTRzMW45NjJxb2R2Z2thOWF1YiJ9.szIAeMS4c9YTgNsJeG36gg"
+              attribution='© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>'
+              tileSize={512}
+              zoomOffset={-1}
+            />
+          </LayersControl.BaseLayer>
+        </LayersControl>
+        <EsriLeafletGeoSearch useMapBounds={false} position="topleft" />
+        <Layers reactTable={reactTable} setSelected={setSelected} setLatLng={setLatLng} />
+        {selectedAttr ? <ZoningPopup selectedAttr={selectedAttr} latLng={popupLatLng} /> : ''}
+        <ZoomControl position="bottomright" />
+      </MapContainer>
+      <a href="http://mapbox.com/about/maps" className="map__watermark" target="_blank">Mapbox</a>
+      <div className="map__legend">
+        Legend
+      </div>
     </div>
-  </div>
-)}
+  );
+};
 
 export default LeafletMap;
--- src/components/map/Popup.jsx
+++ src/components/map/Popup.jsx
@@ -1,22 +1,105 @@
 import React from 'react';
-import { FeatureGroup, Popup } from 'react-leaflet';
+import PropTypes from 'prop-types';
+import { Popup } from 'react-leaflet';
+import * as d3 from 'd3-format';
+import setZoneUse from '../../utils/setZoneUse';
+import setMultifamily from '../../utils/setMultiFamily';
 
-const Popup = ({ selectedAttr, latLng }) => (
+function checkValue(input) {
+  if (input) {
+    return input;
+  }
+  return 'Data not available';
+}
+
+function checkEsval(input) {
+  if (input === 1) {
+    return '*';
+  }
+  return '';
+}
+
+const ZoningPopup = ({
+  latLng, selectedAttr: {
+    muni_name, ZO_NAME_1, ZO_USETY_1, ZO_USEDE, MULTIFAM, MNLS_EFF_1, PCTLOTCOV, LApDU, MXHT_EFF_1, MXDU_EFF_1, DUpAC_EFF_1, FAR_EFF_1, FAR_ESVAL, REVIEWDATE,
+  },
+}) => (
   <Popup position={latLng}>
-    <p>{selectedAttr.muni_name}</p>
-    <p>{selectedAttr.ZO_NAME_1}</p>
-    <p>{selectedAttr.ZO_USETY_1}</p>
-    <p>{selectedAttr.ZO_USEDE}</p>
-    <p>{selectedAttr.MULTIFAM}</p>
-    <p>{selectedAttr.MNLS_EFF_1}</p> {/* MNLS_EFF_ESVAL */}
-    <p>{selectedAttr.PCTLOTCOV}</p> {/* PCTLOTCOV_ESVAL */}
-    <p>{selectedAttr.LApDU}</p>
-    <p>{selectedAttr.MXHT_EFF_1}</p> {/* MXHT_EFF_ESVAL */}
-    <p>{selectedAttr.MXDU_EFF_1}</p> {/* MXDU_EFF_ESVAL */}
-    <p>{selectedAttr.DUpAC_EFF_1}</p> {/* DUpAC_EFF_ESVAL */}
-    <p>{selectedAttr.FAR_EFF_1} {selectedAttr.FAR_ESVAL}</p>
-    <p>{selectedAttr.REVIEWDATE}</p> {/* spatialrec */}
+    <h3 className="popup__title">
+      <span className="popup__title popup__title--muni">{muni_name}</span>
+      { ZO_NAME_1 ? (
+        <>
+          <br />
+          <span className="popup__title popup__title--zoname">{ZO_NAME_1}</span>
+        </>
+      )
+        : '' }
+    </h3>
+    <hr />
+    <ul className="popup__list">
+      <li className="popup__item">
+        <span className="popup__category">Zone Use:</span>
+        {' '}
+        <span className="popup__datum">
+          {setZoneUse(ZO_USETY_1)}
+          {ZO_USEDE ? ` (${ZO_USEDE})` : ''}
+        </span>
+      </li>
+      <li className="popup__item">
+        <span className="popup__category">Multifamily Housing:</span>
+        {' '}
+        <span className="popup__datum">{checkValue(setMultifamily(MULTIFAM))}</span>
+      </li>
+      <li className="popup__item">
+        <span className="popup__category">Minimum Lot Size, Effective (sf):</span>
+        {' '}
+        <span className="popup__datum">{checkValue(d3.format(',')(MNLS_EFF_1))}</span>
+      </li>
+      <li className="popup__item">
+        <span className="popup__category">Percent Lot Coverage, Effective:</span>
+        {' '}
+        <span className="popup__datum">{checkValue(PCTLOTCOV)}</span>
+      </li>
+      <li className="popup__item">
+        <span className="popup__category">Minimum Lot Area per Dwelling Unit (sf):</span>
+        {' '}
+        <span className="popup__datum">{checkValue(LApDU)}</span>
+      </li>
+      <li className="popup__item">
+        <span className="popup__category">Maximum Height, Effective (sf):</span>
+        {' '}
+        <span className="popup__datum">{checkValue(MXHT_EFF_1)}</span>
+      </li>
+      <li className="popup__item">
+        <span className="popup__category">Maximum Dwelling Units, Effective:</span>
+        {' '}
+        <span className="popup__datum">{checkValue(MXDU_EFF_1)}</span>
+      </li>
+      <li className="popup__item">
+        <span className="popup__category">Maximum Dwelling Units per Acre, Effective:</span>
+        {' '}
+        <span className="popup__datum">{checkValue(DUpAC_EFF_1)}</span>
+      </li>
+      <li className="popup__item">
+        <span className="popup__category">Floor-Area Ratio, Effective:</span>
+        {' '}
+        <span className="popup__datum">
+          { checkValue(FAR_EFF_1) }
+          { checkEsval(FAR_ESVAL) }
+        </span>
+      </li>
+      <li className="popup__item">
+        <span className="popup__category">Spatial data received </span>
+        {' '}
+        <span className="popup__datum">{REVIEWDATE}</span>
+      </li>
+    </ul>
   </Popup>
 );
 
-export default Popup;
+ZoningPopup.propTypes = {
+  selectedAttr: PropTypes.object.isRequired,
+  latLng: PropTypes.object.isRequired,
+};
+
+export default ZoningPopup;
