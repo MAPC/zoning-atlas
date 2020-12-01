8896ee50bd4b662b856a757d6b4b09ca565554df
Annabelle Thomas Taylor <ataylor@mapc.org>
2020-12-01 12:07:44 -0500

    Move form modal into DataContainer

--- src/components/EditsForm.jsx
+++ src/components/EditsForm.jsx
@@ -1,85 +1,106 @@
 import React from 'react';
 import { X } from 'phosphor-react';
 
-const EditsForm = () => (
+const EditsForm = ({ setFormIsOpen }) => (
   <>
     <div className="edits__header">
       Edit Zone Info
-      <X size="1rem" />
+      <button
+        type="button"
+        onClick={() => setFormIsOpen(false)}
+        className="edits__exit"
+      >
+        <X size="1rem" />
+      </button>
     </div>
-    <form className="edits__form">
-      <fieldset className="edits__section">
-        <label htmlFor="name" className="edits__row">
-          Name
-          <input name="name" className="edits__input" required />
-        </label>
-        <label htmlFor="email" className="edits__row">
-          Email
-          <input name="email" className="edits__input" required />
-        </label>
-      </fieldset>
-      <fieldset className="edits__section">
-        <label htmlFor="zo_name" className="edits__row">
-          Zone Name
-          <input name="zo_name" className="edits__input" />
-        </label>
-        <label htmlFor="zo_name" className="edits__row">
-          Zone Use Type
-          <select name="zo_name" className="edits__select">
-            <option value="0">No Zone</option>
-            <option value="1">Residential</option>
-            <option value="2">Non-Residential</option>
-            <option value="3">Mixed Use (Residential and Non-Residential)</option>
-            <option value="4">Other (Open space, etc.)</option>
-          </select>
-        </label>
-        <label htmlFor="zo_usede" className="edits__row edits__row--multi-line">
-          Zone Use Description
-          <textarea name="zo_usede" className="edits__textarea" />
-        </label>
-        <label htmlFor="multifam" className="edits__row">
-          Multifamily Housing
-          <select name="multifam" className="edits__select">
-            <option value="0">No</option>
-            <option value="1">By special permit</option>
-            <option value="2">By right</option>
-          </select>
-        </label>
-        <label htmlFor="minlotsize" className="edits__row">
-          Minimum Lot Size (sf)
-          <input type="number" name="minlotsize" min="0" className="edits__input" />
-        </label>
-        <label htmlFor="pctlotcov" className="edits__row">
-          Percent Lot Coverage
-          <input type="number" name="pctlotcov" min="0" max="100" className="edits__input" />
-        </label>
-        <label htmlFor="lapdu" className="edits__row">
-          Minimum Lot Area per Dwelling Unit (sf)
-          <input type="number" name="lapdu" min="0" className="edits__input" />
-        </label>
-        <label htmlFor="maxflrs" className="edits__row">
-          Maximum Building Floors
-          <input type="number" name="mxflrs" min="0" className="edits__input" />
-        </label>
-        <label htmlFor="maxheight" className="edits__row">
-          Maximum Height (ft)
-          <input type="number" name="mxheight" min="0" className="edits__input" />
-        </label>
-        <label htmlFor="maxdu" className="edits__row">
-          Maximum Dwelling Units
-          <input type="number" name="maxdu" min="0" className="edits__input" />
-        </label>
-        <label htmlFor="dupac" className="edits__row">
-          Maximum Dwelling Units per Acre
-          <input type="number" name="dupac" min="0" className="edits__input" />
-        </label>
-        <label htmlFor="far" className="edits__row">
-          Floor-Area Ratio
-          <input type="number" name="far" min="0" className="edits__input" />
-        </label>
-      </fieldset>
-      <button type="submit">Submit</button>
-    </form>
+    <div className="edits__form-wrapper">
+      <form className="edits__form">
+        <p className="edits__paragraph">
+          <span className="edits__asterisk">*</span>
+          {' '}
+          Indicates required field
+        </p>
+        <fieldset className="edits__section">
+          <label htmlFor="name" className="edits__row">
+            <span className="edits__label">
+              <span className="edits__asterisk">*</span>
+              {' '}
+              Name
+            </span>
+            <input name="name" className="edits__input edits__field" required />
+          </label>
+          <label htmlFor="email" className="edits__row">
+            <span className="edits__label">
+              <span className="edits__asterisk">*</span>
+              {' '}
+              Email
+            </span>
+            <input name="email" className="edits__input edits__field" required />
+          </label>
+        </fieldset>
+        <fieldset className="edits__section">
+          <label htmlFor="zo_name" className="edits__row">
+            Zone Name
+            <input name="zo_name" className="edits__input edits__field" />
+          </label>
+          <label htmlFor="zo_name" className="edits__row">
+            Zone Use Type
+            <select name="zo_name" className="edits__select edits__field">
+              <option value="0">No Zone</option>
+              <option value="1">Residential</option>
+              <option value="2">Non-Residential</option>
+              <option value="3">Mixed Use (Residential and Non-Residential)</option>
+              <option value="4">Other (Open space, etc.)</option>
+            </select>
+          </label>
+          <label htmlFor="zo_usede" className="edits__row edits__row--multi-line">
+            Zone Use Description
+            <textarea name="zo_usede" className="edits__textarea edits__field" />
+          </label>
+          <label htmlFor="multifam" className="edits__row">
+            Multifamily Housing
+            <select name="multifam" className="edits__select edits__field">
+              <option value="0">No</option>
+              <option value="1">By special permit</option>
+              <option value="2">By right</option>
+            </select>
+          </label>
+          <label htmlFor="minlotsize" className="edits__row">
+            Minimum Lot Size (sf)
+            <input type="number" name="minlotsize" min="0" className="edits__input edits__field" />
+          </label>
+          <label htmlFor="pctlotcov" className="edits__row">
+            Percent Lot Coverage
+            <input type="number" name="pctlotcov" min="0" max="100" className="edits__input edits__field" />
+          </label>
+          <label htmlFor="lapdu" className="edits__row">
+            Minimum Lot Area per Dwelling Unit (sf)
+            <input type="number" name="lapdu" min="0" className="edits__input edits__field" />
+          </label>
+          <label htmlFor="maxflrs" className="edits__row">
+            Maximum Building Floors
+            <input type="number" name="mxflrs" min="0" className="edits__input edits__field" />
+          </label>
+          <label htmlFor="maxheight" className="edits__row">
+            Maximum Height (ft)
+            <input type="number" name="mxheight" min="0" className="edits__input edits__field" />
+          </label>
+          <label htmlFor="maxdu" className="edits__row">
+            Maximum Dwelling Units
+            <input type="number" name="maxdu" min="0" className="edits__input edits__field" />
+          </label>
+          <label htmlFor="dupac" className="edits__row">
+            Maximum Dwelling Units per Acre
+            <input type="number" name="dupac" min="0" className="edits__input edits__field" />
+          </label>
+          <label htmlFor="far" className="edits__row">
+            Floor-Area Ratio
+            <input type="number" name="far" min="0" className="edits__input edits__field" />
+          </label>
+        </fieldset>
+        <button type="submit" className="button edits__button">Submit</button>
+      </form>
+    </div>
   </>
 );
 
--- src/components/data/DataContainer.jsx
+++ src/components/data/DataContainer.jsx
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ReactModal from 'react-modal';
 import TableFilters from '../filters/TableFilters';
 import TableWrapper from '../table/TableWrapper';
 import LeafletMap from '../map/LeafletMap';
+import EditsForm from '../EditsForm';
 
-const DataContainer = ({ reactTable, view }) => (
-  <div className="data-container">
-    <TableFilters
-      headerGroups={reactTable.headerGroups}
-    />
-    {view === 'tabular'
-      ? <TableWrapper reactTable={reactTable} />
-      : <LeafletMap reactTable={reactTable} />}
-  </div>
-);
+const DataContainer = ({ reactTable, view, appRef }) => {
+  const [formIsOpen, setFormIsOpen] = useState(true);
+  return (
+    <div className="data-container">
+      <TableFilters
+        headerGroups={reactTable.headerGroups}
+      />
+      {view === 'tabular'
+        ? <TableWrapper reactTable={reactTable} />
+        : <LeafletMap reactTable={reactTable} />}
+      <ReactModal
+        isOpen={formIsOpen}
+        contentLabel="Edit form"
+        overlayClassName="modal__overlay modal__overlay--dark"
+        className="modal__content-wrapper edits__wrapper"
+        appElement={appRef}
+      >
+        <EditsForm
+          setFormIsOpen={setFormIsOpen}
+        />
+      </ReactModal>
+    </div>
+  );
+};
 
 export default DataContainer;
