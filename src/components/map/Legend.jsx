3ecef8fe69d0d9a1e4d41bbc1cabd4e39db087ac
Annabelle Thomas Taylor <ataylor@mapc.org>
2020-12-04 16:34:01 -0500

    Toggle between legend on/off

--- src/components/map/Legend.jsx
+++ src/components/map/Legend.jsx
@@ -3,28 +3,34 @@ import { X } from 'phosphor-react';
 import ChoroplethSelect from './ChoroplethSelect';
 
 const Legend = ({ layerStyle, setLayerStyle }) => {
-  const [isVisible, setVisibility] = useState(true);
+  const [isVisible, setVisibility] = useState(false);
   return (
     <div
       className={isVisible ? 'legend legend--active' : 'legend'}
-      onClick={() => setVisibility(true)}
-      onKeyDown={(e) => {
-        if (e.key === 'Enter') {
-          setVisibility(true);
-        }
-      }}
-      tabIndex={0}
-      role="button"
     >
-      <div className="legend__header">
-        <h3 className="legend__title">Legend</h3>
-        <button
-          type="button"
-          onClick={() => setVisibility(false)}
-          className="edits__exit"
+      <div className={isVisible ? 'legend__header legend__header--active' : 'legend__header'}>
+        <h3
+          className={isVisible ? 'legend__title legend__title--active' : 'legend__title'}
+          onClick={() => setVisibility(true)}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              setVisibility(true);
+            }
+          }}
+          tabIndex={0}
+          role="button"
         >
-          <X size="1rem" />
-        </button>
+          Legend
+        </h3>
+        { isVisible ? (
+          <button
+            type="button"
+            onClick={() => setVisibility(false)}
+            className="edits__exit"
+          >
+            <X size="1rem" />
+          </button>
+        ) : ''}
       </div>
       { isVisible ? <ChoroplethSelect layerStyle={layerStyle} setLayerStyle={setLayerStyle} /> : ''}
     </div>
