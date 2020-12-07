76ffc9ed9b51a3c86df751459eb8ed2c405e422f
Annabelle Thomas Taylor <ataylor@mapc.org>
2020-12-07 10:49:04 -0500

    Style pagination buttons

--- src/components/table/TablePagination.jsx
+++ src/components/table/TablePagination.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
+import {
+  CaretLeft, CaretDoubleLeft, CaretRight, CaretDoubleRight,
+} from 'phosphor-react';
 
 const TablePagination = ({
   gotoPage, previousPage, canPreviousPage, nextPage, canNextPage, pageIndex, pageCount,
 }) => (
   <div className="pagination__wrapper">
-    <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
-      {'<<'}
+    <button onClick={() => gotoPage(0)} disabled={!canPreviousPage} className="button button--pagination">
+      <CaretDoubleLeft
+        size="1rem"
+        weight="bold"
+      />
     </button>
     {' '}
-    <button onClick={() => previousPage()} disabled={!canPreviousPage}>
-      {'<'}
+    <button onClick={() => previousPage()} disabled={!canPreviousPage} className="button button--pagination">
+      <CaretLeft
+        size="1rem"
+        weight="bold"
+      />
     </button>
     {' '}
-    <span>
+    <span className="pagination__text">
       Page
       {' '}
       {pageIndex + 1}
@@ -21,11 +30,17 @@ const TablePagination = ({
       {' '}
       {pageCount}
     </span>
-    <button onClick={() => nextPage()} disabled={!canNextPage}>
-      {'>'}
+    <button onClick={() => nextPage()} disabled={!canNextPage} className="button button--pagination">
+      <CaretRight
+        size="1rem"
+        weight="bold"
+      />
     </button>
-    <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
-      {'>>'}
+    <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage} className="button button--pagination">
+      <CaretDoubleRight
+        size="1rem"
+        weight="bold"
+      />
     </button>
     {' '}
   </div>
