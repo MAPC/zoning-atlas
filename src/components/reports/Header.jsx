26290b56c4639e8a1beace8c8a160839bd487f9a
Annabelle Thomas Taylor <ataylor@mapc.org>
2020-12-10 15:28:43 -0500

    Style title box

--- src/components/reports/Header.jsx
+++ src/components/reports/Header.jsx
@@ -4,9 +4,14 @@ import headerImage from '../../images/Antique-Homes-Triple-Decker-Pair.jpg';
 const Header = () => (
   <header className="report__header-wrapper">
     <div className="report__header">
-      <div className="report__title-box">
-        <p className="report__date">December 2020</p>
-        <h2 className="report__title">MAPC Zoning Atlas</h2>
+      <div className="report__navigation-box">
+        <div className="report__title-box">
+          <p className="report__date">December 2020</p>
+          <h2 className="report__title">The MAPC Zoning Atlas</h2>
+        </div>
+        <nav className="report__navigation">
+          About
+        </nav>
       </div>
       <div className="report__hero-wrapper">
         <div className="report__hero" />
