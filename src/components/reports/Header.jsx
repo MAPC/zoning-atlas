f9969b4e5cb06fb4d9673c83a51e8d0047c40aa5
Annabelle Thomas Taylor <ataylor@mapc.org>
2020-12-10 16:19:17 -0500

    Create table of contents

--- src/components/reports/Header.jsx
+++ src/components/reports/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import headerImage from '../../images/Antique-Homes-Triple-Decker-Pair.jpg';
+import { AnchorLink } from 'gatsby-plugin-anchor-links';
+import triangle from '../../images/triangleDownGreen.svg';
 
 const Header = () => (
   <header className="report__header-wrapper">
@@ -10,7 +11,26 @@ const Header = () => (
           <h2 className="report__title">The MAPC Zoning Atlas</h2>
         </div>
         <nav className="report__navigation">
-          About
+          <ul className="report__list">
+            <li className="report__list-item">
+              <img src={triangle} className="report__triangle" />
+            </li>
+            <li className="report__list-item">
+              <img src={triangle} className="report__triangle" />
+              <hr className="report__line" />
+              <AnchorLink to="/reports/0/#introduction" className="report__link">Introduction</AnchorLink>
+            </li>
+            <li className="report__list-item">
+              <img src={triangle} className="report__triangle" />
+              <hr className="report__line" />
+              <AnchorLink to="/reports/0/#middle" className="report__link">Middle</AnchorLink>
+            </li>
+            <li className="report__list-item">
+              <img src={triangle} className="report__triangle" />
+              <hr className="report__line" />
+              <AnchorLink to="/reports/0/#conclusion" className="report__link">Conclusion</AnchorLink>
+            </li>
+          </ul>
         </nav>
       </div>
       <div className="report__hero-wrapper">
