711674b7a7bd7efbd8b04bea149ca291f7e87977
Annabelle Thomas Taylor <ataylor@mapc.org>
2020-12-10 16:58:39 -0500

    Create About section

---
+++ src/components/reports/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const About = () => (
+  <div className="report-about">
+    <hr className="report-about__line" />
+    <div className="report-about__content">
+      <h3 id="about" className="report-about__title">About the Zoning Atlas</h3>
+      <p className="report-about__paragraph">
+        In Massachusetts, every city and town establishes its own zoning regulations and in doing so makes decisions about where and how development can happen—decisions that impact everyone living in or visiting our region. New apartments, commercial areas, industrial parks, and remote subdivisions—all of these are shaped by the zoning boundaries and bylaws or ordinances that a municipality establishes. While zoning is but one component of municipal land use control, it is a powerful one, and a functional representation of a community’s values. Collectively, these many local rules shape the future of the entire region. Seeing how they fit together across municipal boundaries is essential to building a brighter future for all of us. Yet the rules and regulations of Massachusetts municipal zoning codes are so complex, varied, hard to access, and bewildering to interpret that the only available data is decades old and incomplete. MAPC's Zoning Atlas, a resource years in the making, is the first regional zoning map since 1999 and the first to include information about multifamily housing, residential density, commercial density, and overlay districts. Even more important, it is being published as a dynamic online resource that will improve over time as municipal staff and other contributors refine the data and provide updates.
+    </p>
+    </div>
+  </div>
+);
+
+export default About;
