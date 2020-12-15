06a45d941d836bca7c3bfddbdd9afe0e969ffe12
Annabelle Thomas Taylor <ataylor@mapc.org>
2020-12-15 10:00:53 -0500

    Rough draft of Report 0 callout box

--- src/pages/reports.js
+++ src/pages/reports.js
@@ -7,6 +7,8 @@ import ExternalSiteCell from '../components/reports/ExternalSiteCell';
 import report1 from '../images/report1.png';
 import report2 from '../images/report2.png';
 import report3 from '../images/report3.png';
+import report0 from '../images/Antique-Homes-Triple-Decker-Pair-cropped.png';
+import triangleDownGreen from '../images/triangleDownGreen.svg';
 
 const Reports = ({ data }) => {
   const externalSites = data.allMarkdownRemark.nodes.map((node) => (
@@ -19,6 +21,16 @@ const Reports = ({ data }) => {
     <Layout title="Reports - MAPC Zoning Atlas">
       <main className="main__wrapper">
         <div className="main__wrapper--narrow">
+        <section style={{ backgroundColor: '#F0EFE7', display: 'flex', padding: '1rem', marginBottom: '60px' }}>
+        <img src={triangleDownGreen} className="callout-box__triangle" />
+          <div>
+            <h2>Introducting the Zoning Atlas</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Usu ut commune mentitum, putent facete vim id. Scripta periculis ei eam, te pro movet reformidans. Scripta periculis ei eam, te pro movet reformidans. Vivendum intellegat et qui, ei denique consequuntur vix. Harum repudiandae sea at. Et clita interesset quo. Offendit eleifend moderatius ex vix, quem odio mazim et qui, purto expetendis cotidieque quo cu, veri persius vituperata ei nec. Ei qui diceret voluptua luptatum, te dolorum detracto hendrerit sed, ad per offendit consetetur. Semper aeterno percipit ut his, sea ex utinam referrentur repudiandae. Vivendum intellegat et qui, ei denique consequuntur vix.
+            </p>
+          </div>
+          <img src={report0} style={{ width: '300px', height: '366px', marginLeft: '32px'}}/>
+        </section>
           <h2>Explore Related Reports & Publications</h2>
           <section className="report-link__marquee">
             <ReportLink
