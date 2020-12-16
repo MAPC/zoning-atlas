f92c61646ac734aeb96e51f72ded0d68e8338d08
Annabelle Thomas Taylor <ataylor@mapc.org>
2020-12-16 09:55:01 -0500

    Fix About Zoning Atlas section of report 0
    Now that the about section has multiple paragraphs, we need to read in the content and put its dangerouslySetInnerHTML in a div, not another p tag

--- src/components/reports/About.jsx
+++ src/components/reports/About.jsx
@@ -5,7 +5,7 @@ const About = ({ content }) => (
     <hr className="report-about__line" />
     <div className="report-about__content">
       <h3 id="about" className="report-about__title">{content.frontmatter.title}</h3>
-      <p className="report-about__paragraph" dangerouslySetInnerHTML={{ __html: content.html }} />
+      <div className="report-about__paragraph" dangerouslySetInnerHTML={{ __html: content.html }} />
     </div>
   </section>
 );
